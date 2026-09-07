8c3440a8a5f59b9baf206c6bc9df1060c567f97a
Deepak negi <dnegi5817@gamil.com>
2026-09-08 00:16:50 +0530

    update

--- src/binarySearchTree/basicsOfBST.java
+++ src/binarySearchTree/basicsOfBST.java
@@ -15,6 +15,26 @@ class TreeNode {
 }
 
 public class basicsOfBST {
+	public int[] morrisTra(TreeNode root) {
+
+	}
+
+	public int[] inorderPredecesor(TreeNode root, int k) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		inOrderTra(root, arr);
+		int i = 0;
+		int j = arr.size();
+		int mid = 0;
+		while (i < j) {
+			mid = (i - j) / 2 + j;
+			if (arr.get(mid) < k) i = mid + 1;
+			else if (arr.get(mid) > k) j = mid - 1;
+			else break;
+		}
+		int[] ans = {arr.get(mid - 1), arr.get(mid + 1)};
+		return ans;
+	}
+
 	//delete the node
 	public TreeNode right(TreeNode root) {
 		if (root.right.right == null) return root;
@@ -67,6 +87,7 @@ public class basicsOfBST {
 
 	//sorted order arraylist
 	public static void inOrderTra(TreeNode root, ArrayList<Integer> arr) {
+
 		if (root == null) return;
 		inOrderTra(root.left, arr);
 		arr.add(root.val);
